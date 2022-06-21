115afe5f86663961f5d77cecdb0b6d9b82b14a3c
Nico H <21085229+exobite@users.noreply.github.com>
2022-06-21 18:37:23 +0200

    Version 0.1.7 Release just removed two debug Messages and fixed a typo in changelog.txt

--- src/main/com/github/exobite/mc/playtimerewards/main/PluginMaster.java
+++ src/main/com/github/exobite/mc/playtimerewards/main/PluginMaster.java
@@ -124,11 +124,9 @@ public class PluginMaster extends JavaPlugin {
                 pauseAsyncTimer = true; //Pause the Clock while reloading data
                 Config.reloadConfig(true);
                 Lang.reloadLang();
-                System.out.println(RewardManager.getInstance().getRegisteredRewardData().size());
                 if(!RewardManager.reloadRewards(forceRewardReload) && forceRewardReload) {
                     sendConsoleMessage(Level.WARNING, "Couldn't reload the Rewards while they are being edited.");
                 }
-                System.out.println(RewardManager.getInstance().getRegisteredRewardData().size());
                 AFKManager.getInstance().reloadConfig();
                 ExoDebugTools.unregister();
                 if(Config.getInstance().allowDebugTools()) ExoDebugTools.registerDebugTools(instance);
